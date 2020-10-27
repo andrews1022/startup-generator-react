6db1a804f57d0100c4dec0251eb4c4352f0c7cf3
andrews1022 <andrewshearerphotography@gmail.com>
2020-10-26 17:08:56 -0700

    quick file formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer>
-    <p>Copyright &#169; {new Date().getFullYear()} by Andrew Shearer</p>
-  </footer>
+	<footer>
+		<p>Copyright &#169; {new Date().getFullYear()} by Andrew Shearer</p>
+	</footer>
 );
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Header = () => (
-  <header className='header'>
-    <h1 className='header__title'>Startup Generator</h1>
-    <p className='header__tagline'>
-      Got developers block? Use this site to generate a random startup idea, and get building!
-    </p>
-  </header>
+	<header className='header'>
+		<h1 className='header__title'>Startup Generator</h1>
+		<p className='header__tagline'>
+			Got developers block? Use this site to generate a random startup idea, and get building!
+		</p>
+	</header>
 );
 
 export default Header;
